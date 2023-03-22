b82792bcd6aebf2864fa4bbe4e78ed33b2fbe512
richiebailey74 <57724969+richiebailey74@users.noreply.github.com>
2023-03-22 00:36:06 -0400

    sample

---
+++ src/simulation_test.py
@@ -0,0 +1,7 @@
+from datamodel import Listing, OrderDepth, Trade, TradingState
+import numpy as np
+import pandas as pd
+
+
+# this file will contain the logic that will simulate our algorithm stacked against the training data (bots and market trades)
+
